e328ee4758094c105798c78e6039c8c99ee4bb2a
Stefan So <34347838+Steve2312@users.noreply.github.com>
2021-10-08 17:46:27 +0200

    Fix: project is now buildable

--- src/main/java/DAO/ThemeDAO.java
+++ src/main/java/DAO/ThemeDAO.java
@@ -4,9 +4,9 @@ public class ThemeDAO {
     private static ThemeDAO themeDAO;
 
     //Todo what does this return bruh ?
-    public Theme getTheme(){
-        return
-    }
+//    public Theme getTheme(){
+//        return
+//    }
 
     //Todo
     public static ThemeDAO getInstance() {
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,12 +1,13 @@
 package controller;
 
+import DAO.LoginDAO;
 import model.Login;
 
 public class LoginController {
 
     private static LoginController loginController;
     //Todo
-    private LoginService loginService;
+    private LoginDAO loginDAO;
     private Login login;
 
     public void login(String username, String password){}
