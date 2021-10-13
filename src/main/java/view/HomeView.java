a8065e8102558cbcb13cc6df5a3346d30204fe8f
Moviescar <73886068+Moviescar@users.noreply.github.com>
2021-10-13 11:29:31 +0200

    homeview update

--- src/main/java/view/HomeView.java
+++ src/main/java/view/HomeView.java
@@ -1,14 +1,13 @@
 package view;
 
-import controller.HomeController;
-import controller.ThemeController;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -17,38 +16,77 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class HomeView {
-    private HomeController homeController;
-    private ThemeController themeController;
+    final double buttonPadding = 17.5;
+    final int headerFontSize = 64;
+    final int buttonFontSize = 22;
 
+    final int logoWidth = 480;
+    final int logoHeight = 128;
+
+    final int gridHGap = 75;
+    final int gridVGap = 50;
+    final String buttonColor = "#9CC2D4";
+    final String hoverButtonColor = "#E4F6FF";
+
+    final String logoutButtonColor = "#FFFFFF";
+
+    final String fontFamily = "Arial";
     public Scene homeScreen() throws FileNotFoundException {
+
         BackgroundSize backgroundSize = new BackgroundSize(1280, 800, true, true, true, false);
-        BackgroundImage background = null;
+        BackgroundImage bgImage = null;
         try {
-            background = new BackgroundImage(new Image(new FileInputStream("./src/main/resources/background.png")),
+            bgImage = new BackgroundImage(new Image(new FileInputStream("./src/main/resources/background.png")),
                     BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Image logo = new Image(new FileInputStream("./src/main/resources/logo.png"),480, 127, false, false);
-        ImageView logoView = new ImageView(logo);
+        FileInputStream inputstream = new FileInputStream("src/main/resources/logo.png");
+        Image logoImage = new Image(inputstream, logoWidth, logoHeight, false, false);
+        ImageView logoView = new ImageView(logoImage);
 
         HBox logoContainer = new HBox(25);
-        logoContainer.getChildren().add(logoView);
-        logoContainer.setLayoutX(300);
-        logoContainer.setLayoutY(300);
+        logoContainer.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        logoContainer.setPadding(new Insets(20, 0, 0,20));
+        logoContainer.getChildren().addAll(logoView);
 
-        Text Title = new Text();
-        Title.setText("Weten welke subsidie bij jou past?");
-        Title.setFont(Font.font("Arial",FontWeight.BOLD,48));
-        Title.setFill(Color.WHITE);
 
-        VBox titleContainer = new VBox(25);
-        titleContainer.setAlignment(Pos.CENTER);
-        titleContainer.getChildren().add(Title);
+        VBox TitleContainer = new VBox(25);
+        TitleContainer.setPadding(new Insets(0,0,0,40));
+        TitleContainer.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        Text title = new Text("Weten welke subsidie bij jou past?");
+        title.setFont(Font.font (fontFamily, FontWeight.BOLD, headerFontSize));
+        title.setFill(Color.WHITE);
+
+        Text text = new Text("Maak nu onze vragenlijst");
+        text.setFont(Font.font (fontFamily, FontWeight.BOLD, 34));
+        text.setFill(Color.WHITE);
+
+        TitleContainer.getChildren().addAll(title,text);
+
+        HBox bottomContainer = new HBox(25);
+        Button start_vragenlijst = new Button("Start vragenlijst");
+        start_vragenlijst.setFont(Font.font(fontFamily, FontWeight.BOLD, buttonFontSize));
+        start_vragenlijst.setTextFill(Color.BLACK);
+        start_vragenlijst.setAlignment(Pos.CENTER_LEFT);
+        start_vragenlijst.setPadding(new Insets(buttonPadding));
+        start_vragenlijst.setPrefWidth(326);
+        start_vragenlijst.setStyle(String.format("-fx-background-color: %s;", buttonColor));
+        start_vragenlijst.setOnMouseEntered(e -> {
+            start_vragenlijst.setStyle(String.format("-fx-background-color: %s;", hoverButtonColor));
+        });
+        start_vragenlijst.setOnMouseExited(e -> {
+            start_vragenlijst.setStyle(String.format("-fx-background-color: %s;", buttonColor));
+        });
 
         VBox layout = new VBox(25);
-        layout.setBackground(new Background(background));
-        layout.getChildren().addAll(logoContainer,titleContainer);
+        layout.setSpacing(25);
+        layout.setMinWidth(1000);
+        layout.setBackground(new Background(bgImage));
+        layout.getChildren().addAll(logoContainer,TitleContainer);
+
+
 
         return new Scene(layout, 1280,720);
     }
