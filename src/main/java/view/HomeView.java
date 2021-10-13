2d469002ff3b2806aa2e459af484f7db5b74fb5a
Moviescar <73886068+Moviescar@users.noreply.github.com>
2021-10-13 12:04:39 +0200

    home view mostly done

--- src/main/java/view/HomeView.java
+++ src/main/java/view/HomeView.java
@@ -46,14 +46,14 @@ public class HomeView {
         ImageView logoView = new ImageView(logoImage);
 
         HBox logoContainer = new HBox(25);
-        logoContainer.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+//        logoContainer.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
         logoContainer.setPadding(new Insets(20, 0, 0,20));
         logoContainer.getChildren().addAll(logoView);
 
 
         VBox TitleContainer = new VBox(25);
         TitleContainer.setPadding(new Insets(0,0,0,40));
-        TitleContainer.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+//        TitleContainer.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
 
         Text title = new Text("Weten welke subsidie bij jou past?");
         title.setFont(Font.font (fontFamily, FontWeight.BOLD, headerFontSize));
@@ -65,7 +65,12 @@ public class HomeView {
 
         TitleContainer.getChildren().addAll(title,text);
 
-        HBox bottomContainer = new HBox(25);
+
+        HBox bottomContainer = new HBox(400);
+        bottomContainer.setPadding(new Insets(0, 0, 0,40));
+//        bottomContainer.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+        bottomContainer.setAlignment(Pos.BOTTOM_LEFT);
+        bottomContainer.setPrefHeight(300);
         Button start_vragenlijst = new Button("Start vragenlijst");
         start_vragenlijst.setFont(Font.font(fontFamily, FontWeight.BOLD, buttonFontSize));
         start_vragenlijst.setTextFill(Color.BLACK);
@@ -80,11 +85,20 @@ public class HomeView {
             start_vragenlijst.setStyle(String.format("-fx-background-color: %s;", buttonColor));
         });
 
+        Text madeBy = new Text("De beslissingsmatrix wordt medemogelijk gemaakt door\n" +
+                "het Stimuleringsfonds voor de Journalistiek\n" +
+                "www.svdj.nl");
+        madeBy.setFont(Font.font (fontFamily, FontWeight.BOLD, 16));
+        madeBy.setFill(Color.WHITE);
+
+
+        bottomContainer.getChildren().addAll(start_vragenlijst,madeBy);
+
         VBox layout = new VBox(25);
         layout.setSpacing(25);
         layout.setMinWidth(1000);
         layout.setBackground(new Background(bgImage));
-        layout.getChildren().addAll(logoContainer,TitleContainer);
+        layout.getChildren().addAll(logoContainer,TitleContainer,bottomContainer);
 
 
 
