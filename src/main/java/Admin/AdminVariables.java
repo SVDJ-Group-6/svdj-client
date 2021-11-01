ad082ef4451439ef86b1a686555f4b9bb652720d
Jans3n <jans3nsu@gmail.com>
2021-11-01 13:22:33 +0100

    send email

--- src/main/java/Admin/AdminVariables.java
+++ src/main/java/Admin/AdminVariables.java
@@ -6,6 +6,6 @@ import model.Theme;
 public class AdminVariables {
     static public Stage stage;
     static public String token;
-    public static String API_URL = "http://da35-84-87-167-193.ngrok.io";
+    public static String API_URL = "http://e02e-84-87-167-193.ngrok.io";
     public static Theme theme;
 }
