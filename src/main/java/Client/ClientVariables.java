b62b8f25384343377c394f7aea70709cf25d4a27
Stefan So <34347838+Steve2312@users.noreply.github.com>
2021-11-05 15:24:13 +0100

    API_URL

--- src/main/java/Client/ClientVariables.java
+++ src/main/java/Client/ClientVariables.java
@@ -6,5 +6,5 @@ import model.Theme;
 public class ClientVariables {
     public static Stage stage;
     public static Theme theme;
-    public static String API_URL = "http://b9ad-84-87-167-193.ngrok.io";
+    public static String API_URL = "http://localhost:8080";
 }
